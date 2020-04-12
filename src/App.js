0bcfe867d61937975b7b85ae05ef97654344ad99
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-12 15:21:10 +0100

    creating structure for app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css"
-import uuid from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
 
-function App() {
-    return (
-        <div className="App">
-            <h1>hello from app</h1>
-            <TodoInput/>
-            <TodoList/>
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {id: 1, title: 'wake up'},
+                {id: 2, title: 'make breakfast'}
+            ],
+            id: uuidv4(),
+            item: '',
+            editItem: false
+        };
+    }
+
+    handleChange = (event) => {
+        console.log('handleChange');
+    };
+    handleSubmit = () => {
+        console.log('handleSubmit');
+    };
+
+    clearList = () => {
+        console.log('clear list')
+    };
+
+    handleDelete = (id) => {
+        console.log('handleDelete')
+    };
+
+    handleEdit = (id) => {
+        console.log('handleEdit');
+    };
+
+
+    render() {
+        console.log(this.state);
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-8 mt-5">
+                        <h3 className='text-capitalize text-center'>todo input</h3>
+                        <TodoInput item={this.state.item} handleChange={this.handleChange}
+                                   handleSubmit={this.handleSubmit} editItem={this.state.editItem}/>
+                        <TodoList items={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete}
+                                  handleEdit={this.handleEdit}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
