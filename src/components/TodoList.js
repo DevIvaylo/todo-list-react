a8196fe4612abc6e60d1071a2f55f0629e8c8014
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-12 17:52:49 +0100

    structuring TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,19 @@ import TodoItem from "./TodoItem";
 
 export default class TodoList extends Component {
     render() {
+        const {items, clearList, handleEdit, handleDelete} = this.props;
         return (
-            <h1>Todo list
-                <h2><TodoItem/></h2>
-            </h1>
+            <ul className='list-group my-5'>
+                <h3 className='text-capitalize text-center'>todo list</h3>
+                {items.map(item => <TodoItem key={item.id} title={item.title}
+                                             handleEdit={handleEdit}
+                                             handleDelete={handleDelete}/>)}
+                <button type='button'
+                        className='btn btn-danger btn-block text-uppercase mt-5'
+                        onClick={clearList}>
+                    clear list
+                </button>
+            </ul>
         );
     }
 }
