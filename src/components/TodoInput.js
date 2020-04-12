e0ec2d10dd4758fb0921a4a8442c02ad70ab9c76
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-12 15:40:55 +0100

    creating structure for TodoInput component

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,8 +2,26 @@ import React, {Component} from 'react';
 
 export default class TodoInput extends Component {
     render() {
+        const {item, editItem, handleChange, handleSubmit} = this.props;
         return (
-            <h1>Todo input</h1>
+            <>
+                <div className='card card-body my-3'>
+                    <form onSubmit={handleSubmit}>
+                        <div className='input-group'>
+                            <div className="input-group-prepend">
+                                <div className="input-group-text bg-primary">
+                                    <i className="fas fa-book"/>
+                                </div>
+                            </div>
+                            <input name='item' value={item} onChange={handleChange} placeholder='add todo item'
+                                   className='form-control text-capitalize' type="text"/>
+                        </div>
+                        <button type='submit' className='btn btn-block btn-primary mt-3 text-capitalize'>
+                            add item
+                        </button>
+                    </form>
+                </div>
+            </>
         );
     }
 }
